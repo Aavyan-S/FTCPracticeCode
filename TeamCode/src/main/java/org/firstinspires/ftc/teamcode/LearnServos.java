8e440c23d4e150b50f5710986a732e255cfb9328
Aavyan-S <aavyan.rs@outlook.com>
2025-07-21 15:09:31 -0700

    Change to add Regular Servo as well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearnServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearnServos.java
@@ -3,13 +3,16 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp()
 public class LearnServos extends OpMode {
     public CRServo crServo;
+    public Servo servo;
     public void init()
     {
-        crServo=hardwareMap.get(CRServo.class,"servo");
+        servo=hardwareMap.get(Servo.class,"servo");
+        crServo=hardwareMap.get(CRServo.class,"crservo");
     }
     public void loop()
     {
@@ -18,5 +21,10 @@ public class LearnServos extends OpMode {
             crServo.setPower(1.0);
         }
         crServo.setPower(0.0);
+        if (gamepad1.b)
+        {
+            servo.setPosition(0.5);
+        }
+        servo.setPosition(0.0);
     }
 }
