fb9803ccace6093e0bf05fe4deb91c3a7c8f901d
Aavyan-S <aavyan.rs@outlook.com>
2025-08-18 19:00:12 -0700

    update code lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSampleProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FindSampleProcessor.java
@@ -3,20 +3,23 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Canvas;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
-import java.lang.reflect.Array;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
 import org.firstinspires.ftc.vision.VisionProcessor;
 
 public class FindSampleProcessor implements VisionProcessor {
+    private final Size frameSize = new Size(640, 480);
     private final List<MatOfPoint> contours = new ArrayList<>();
     private final Mat gray = new Mat();
     private final Mat hsv = new Mat();
     private Mat submat = new Mat();
+    private MatOfPoint2f contour2f;
     private final Mat binary = new Mat();
     public void init(int width, int height, CameraCalibration calibration) {
 
@@ -50,7 +53,7 @@ public class FindSampleProcessor implements VisionProcessor {
         List<RotatedRect> contourBounds = new ArrayList<>();
         for (MatOfPoint contour : contours) {
             Point[] points = contour.toArray();
-            MatOfPoint2f contour2f = new MatOfPoint2f(points);
+            contour2f = new MatOfPoint2f(points);
             RotatedRect temp= Imgproc.minAreaRect(contour2f);
             contourBounds.add(temp);
         }
@@ -68,5 +71,46 @@ public class FindSampleProcessor implements VisionProcessor {
         Scalar color=Core.mean(submat);
         return color.val[0];
     }
+    public List<RotatedRect> getContoursBlue() {
+        List<RotatedRect> contoursBlue = new ArrayList<>();
+        for (int i = 0; i < contours.size(); i++) {
+            if (getAvgHue(hsv, getContourCoords().get(i)) > 180 && getAvgHue(hsv, getContourCoords().get(i)) < 275) {
+                contoursBlue.add(getContourCoords().get(i));
+            }
+        }
+        return contoursBlue;
+    }
+    public int[] getContoursDist(List<RotatedRect> contours) {
+        int[] contourDist = new int[contours.size()];
+        int deltaX;
+        int deltaY;
+        int angle;
+        for(int i = 0; i < contours.size(); i++) {
+                deltaX = (int)contours.get(i).center.x - 320;
+                deltaY = (int)contours.get(i).center.y - 240;
+                angle= (int)contours.get(i).angle;
 
+                if (angle > 45 || angle < -45)
+                {
+                    contourDist[i]=0;
+                }
+                else {
+                    contourDist[i] = (int)Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+                }
+        }
+        return contourDist;
+    }
+    public List<RotatedRect> sortContours() {
+        ArrayList<RotatedRect> sortedContours = new ArrayList<>(getContourCoords());
+        sortedContours.sort(Comparator.comparingInt(c -> (int)-c.angle));
+        if (sortedContours.isEmpty()) {
+            return null;
+        }
+        else {
+            return sortedContours;
+        }
+    }
+    public RotatedRect getEasiestContour() {
+        return sortContours().get(0);
+    }
 }
