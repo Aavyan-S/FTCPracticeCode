188d0ccecdf96800604b24f8a2c03a4bcf70f253
Aavyan-S <aavyan.rs@outlook.com>
2025-09-06 15:44:02 -0700

    New code learned during FTC Future Martians coding camp.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCAprilTag.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+public class FTCAprilTag extends LinearOpMode {
+    // Do not use distance sensor with Webcam/Limelight // Farther away = less reliable. More angled = less reliable //Tag size, tag count // Recalibrate position only if reliable // Best to use both Odometry and April Tags together
+    public void runOpMode() {
+        //TFOD //OpenCV // Color blob locator
+        waitForStart();
+    }
+}
