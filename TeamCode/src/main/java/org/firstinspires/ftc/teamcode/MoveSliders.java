331e240a0ddd87f493277dd8ab9330448a8bfed9
Aavyan-S <aavyan.rs@outlook.com>
2025-09-06 15:45:05 -0700

    Change for Elapsed Time add for time limit for the slider retract

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveSliders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveSliders.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class MoveSliders {
     private DcMotor leftSlider;
     private DcMotor rightSlider;
+    private ElapsedTime et = new ElapsedTime();
     public void initialize(HardwareMap hwMap) {
         leftSlider = hwMap.get(DcMotor.class, "leftSlider");
         leftSlider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -41,33 +43,35 @@ public class MoveSliders {
             rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightSlider.setPower(0.3);
             while ((leftSlider.isBusy() || rightSlider.isBusy())) {
-                telemetry.addData("TargetLeft", -1000);
-                telemetry.addData("TargetRight", 1000);
-                telemetry.addData("CurrentLeft", leftSlider.getCurrentPosition());
-                telemetry.addData("CurrentRight", rightSlider.getCurrentPosition());
+//                telemetry.addData("TargetLeft", -1000);
+//                telemetry.addData("TargetRight", 1000);
+//                telemetry.addData("CurrentLeft", leftSlider.getCurrentPosition());
+//                telemetry.addData("CurrentRight", rightSlider.getCurrentPosition());
                 telemetry.update();
             }
             leftSlider.setPower(0);
             rightSlider.setPower(0);
         }
         else {
-            leftSlider.setTargetPosition(1000);
+            leftSlider.setTargetPosition(50);
             leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             leftSlider.setPower(0.3);
 
-            rightSlider.setTargetPosition(-1000);
+            rightSlider.setTargetPosition(-50);
             rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightSlider.setPower(0.3);
-            while ((leftSlider.isBusy() || rightSlider.isBusy())) {
-                telemetry.addData("TargetLeft", 1000);
-                telemetry.addData("TargetRight", -1000);
-                telemetry.addData("CurrentLeft", leftSlider.getCurrentPosition());
-                telemetry.addData("CurrentRight", rightSlider.getCurrentPosition());
+            et.reset();
+            while ((leftSlider.isBusy() || rightSlider.isBusy()) && et.seconds()<=3) {
+//                telemetry.addData("TargetLeft", 50);
+//                telemetry.addData("TargetRight", -50);
+//                telemetry.addData("CurrentLeft", leftSlider.getCurrentPosition());
+//                telemetry.addData("CurrentRight", rightSlider.getCurrentPosition());
                 telemetry.update();
             }
+//            telemetry.addLine("Out of While Loop");
+            telemetry.update();
             leftSlider.setPower(0);
             rightSlider.setPower(0);
-
         }
     }
 //    public void move(int location, DcMotor slider) {
